7a931ed569a4db798db65f1197bb720a5107bf90
fahmeda5660 <fariha.208860@gmail.com>
2023-05-10 12:56:58 +0600

    basic checklist mongodb

---
+++ src/__eslintrc.js
@@ -0,0 +1,2 @@
+  // add node: true for ignoring eslintrs error
+//   env: { browser: true, es2020: true, node: true },
---
+++ src/__steps_mongodb.js
@@ -0,0 +1,33 @@
+
+/**mongodb
+ * https://www.mongodb.com/atlas/database --> signin with google-->accept privacy-->learn mongodb-->exporing-->javascript-->finish
+ *free-->aws-->create 
+ *authentication: user password copy and store in code 
+ *mkdir simple-crud-server-->cd-->npm init -y
+ * npm i express cors mongodb 
+ * package er script e "start":"node index.js",
+ * make file index.js
+ * require express and cors
+ * middleware: 
+ *app.use(cors());
+ * req.body is undefined: body parser/ middleware/ express.json -->app.use(express.json());
+ * others code
+ *nodemon index.js
+ * data service, quickstart -->cloud environment-->finish and close
+ * setup e close
+ * Network access--> add ip add list entry-->allow access anywhere
+ * Database access -->email and pass diye --->add user korte pari
+ * Database --> connect-->drivers-->noodejs-->mongodb install done-->view full code
+ * copy code and paste index.js file before or after get code
+ * and password fix
+ * 
+ * 
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/
+ * https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertOne/
+ * database-->browse collection
+ * find mul doc: cursor : https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
+ * 
+ * https://expressjs.com/en/starter/basic-routing.html
+ * */ 
